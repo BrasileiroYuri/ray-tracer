75a3afb2f0891b6d487dc6005f3896c88f6dc1c4
Yuri Maximiliano Brasileiro Santos <yuri.brasileiro.706@ufrn.edu.br>
2026-05-02 20:54:28 -0300

    Feature: adicionando função li.

--- include/integrators/integrator.hpp
+++ include/integrators/integrator.hpp
@@ -1,11 +1,14 @@
 #ifndef INTEGRATOR_HPP
 #define INTEGRATOR_HPP
 
+#include "background.hpp"
 #include "camera.hpp"
 #include "orthographic_camera.hpp"
 #include "perspective_camera.hpp"
+#include "ray.hpp"
 #include "scene.hpp"
 #include <memory>
+#include <optional>
 #include <string>
 
 struct RenderConfig {};
@@ -14,7 +17,23 @@ class Integrator {
 public:
   virtual ~Integrator() = default;
 
-  virtual void render(const Scene &, RenderConfig = RenderConfig{}) = 0;
+  virtual void render(const Scene &sc, RenderConfig = RenderConfig{}) {
+
+    auto widht = cam_->widht();
+    auto height = cam_->height();
+
+    for (std::size_t j = 0; j < height; j++) {
+      for (std::size_t i = 0; i < widht; i++) {
+        Ray ray = cam_->generateRay(i, j);
+
+        auto color = li(ray, sc).value_or(
+            sc.backGroundColor_.sample(float(i) / float(widht - 1),
+                                       1.f - (float(j) / (float)(height - 1))));
+
+        cam_->add(i, j, color);
+      }
+    }
+  }
 
   void makeCamera(CameraConfig cc) {
     if (cc.type == "perspective")
@@ -29,5 +48,7 @@ public:
 
 protected:
   std::unique_ptr<Camera> cam_ = nullptr;
+
+  virtual std::optional<RGBColor> li(const Ray &ray, const Scene &sc) = 0;
 };
 #endif // !INTEGRATOR_HPP
--- include/integrators/raycast_integrator.hpp
+++ include/integrators/raycast_integrator.hpp
@@ -1,31 +1,19 @@
 #ifndef RAYCAST_INTEGRATOR_HPP
 #define RAYCAST_INTEGRATOR_HPP
 
+#include "background.hpp"
 #include "integrator.hpp"
+#include "scene.hpp"
 
 struct RayCastIntegrator : public Integrator {
-  virtual void render(const Scene &sc, RenderConfig rc) override {
+  std::optional<RGBColor> li(const Ray &ray, const Scene &sc) override {
 
-    auto widht = cam_->widht();
-    auto height = cam_->height();
+    Surfel s;
 
-    for (std::size_t j = 0; j < height; j++) {
-      for (std::size_t i = 0; i < widht; i++) {
-        Ray ray = cam_->generateRay(i, j);
+    if (sc.aggregate_->intersect(ray, s))
+      return s.mat_->getColor();
 
-        Surfel s;
-
-        if (sc.aggregate_->intersect(ray, s)) {
-          if (s.mat_.get())
-            cam_->add(i, j, s.mat_->getColor());
-        } else {
-
-          float u = float(i) / float(widht - 1);
-          float v = 1.f - (float(j) / (float)(height - 1));
-          cam_->add(i, j, sc.backGroundColor_.sample(u, v));
-        }
-      }
-    }
+    return {};
   }
 };
 #endif // !RAYCAST_INTEGRATOR_HPP
