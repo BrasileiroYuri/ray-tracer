85889f3acab3a378b5fce4e80f39a0ace90754bf
Yuri Sants <yurimax322@gmail.com>
2026-05-04 14:27:05 -0300

    Adicionando componentes de BlinnPhong Material e Integrator.

--- include/integrators/blinnphong_integrator.hpp
+++ include/integrators/blinnphong_integrator.hpp
@@ -2,6 +2,8 @@
 #define BLINNPHONG_INTEGRATOR_HPP
 
 #include "integrator.hpp"
-class BlinnPhongIntegrator : public Integrator {};
+class BlinnPhongIntegrator : public Integrator {
+  std::optional<RGBColor> li(const Ray &ray, const Scene &sc) {}
+};
 
 #endif // !BLINNPHONG_INTEGRATOR_HPP
--- include/materials/blinnphong_material.hpp
+++ include/materials/blinnphong_material.hpp
@@ -2,6 +2,13 @@
 #define BLINNPHONG_MATERIAL_HPP
 
 #include "material.hpp"
-class BlinnPhongMaterial : public Material {};
+#include "math.hpp"
+
+class BlinnPhongMaterial : public Material {
+	vec3d kd; /* Diffuse coefficient. */
+	vec3d ks; /* Specular coefficient. */
+	vec3d ka; /* Ambient coefficient. */
+	float glossiness;
+};
 
 #endif // !BLINNPHONG_MATERIAL_HPP
